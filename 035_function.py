3941ebd5f994d56ceb4c7d142d26a2aee1739d23
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2024-12-24 04:36:45 +0800

    basics of functions

---
+++ 035_function.py
@@ -0,0 +1,17 @@
+"""
+function = A block of reusable code, place ( ) after tp invoke it
+"""
+
+def wish(name):
+    print(f"Hi {name}")
+    print("I wish you love")
+    print()
+
+for i in range(0,3):
+    wish("you")
+
+def age(num):
+    print(f"I know its scary to turn {num}")
+    print("But Im proud of you")
+
+age(26)
