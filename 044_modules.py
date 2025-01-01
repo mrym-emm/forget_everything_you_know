c7b4d4988fdad72f55ef4702c597f6c53ccd1135
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-01-01 11:12:31 +0800

    basic showing of how to use modules. exmodules.py

---
+++ 044_modules.py
@@ -0,0 +1,16 @@
+"""
+modules = a file containing code you want to include in your program
+          use 'import' to inc a module (built-in or ur own)
+          useful to break up a large program resuable seperate files
+"""
+
+# print(help("math"))
+# print(help("modules"))
+
+
+# can import my own modules
+import exmodules
+
+result = exmodules.square(3)
+
+print(result)
---
+++ exmodules.py
@@ -0,0 +1,7 @@
+pi = 3.14
+
+def square(x):
+    return x**2
+
+def area(radius):
+    return pi * radium**2
