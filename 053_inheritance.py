13c3b0d3b9441b63152f478f0f485a3b2c142aa5
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-01-06 16:23:36 +0800

    intro to inheritance

---
+++ 053_inheritance.py
@@ -0,0 +1,50 @@
+"""
+inheritance = Allows a class to inherit attributes and methods from another class
+              Helps with code reusability and extensibility
+              class Child(Parent)
+"""
+
+class Animal:
+
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is asleep")
+
+# So the Dog class is gonna inherit from the Animal class
+class Dog(Animal):
+    def speak(self):
+        print("WOOF")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW")
+
+class Mouse(Animal):
+    def speak(self):
+        print("SQUEEK")
+
+
+dog = Dog("Scooby")
+cat = Cat("Garfield")
+mouse = Mouse("Mickey")
+
+animal_list = [dog, cat, mouse]
+
+for animal in animal_list:
+    print(animal.name)
+    print(animal.is_alive)
+
+    animal.eat()
+    animal.sleep()
+    animal.speak()
+
+    print()
+
+
+
